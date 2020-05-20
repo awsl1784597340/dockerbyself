5c908faee68aeb2dab4591884772dfc1894fb66f
awsl1784597340 <grandgadsix@sjtu.edu.cn>
2020-05-20 15:12:50 +0800

    'clear'

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,14 @@
+//原先的
 import Vue from 'vue'
 import Router from 'vue-router'
-import Experiment from "../components/Student/Experiment"
-import ListExperiments from "../components/Student/ListExperiments"
-import AddExperiments from "../components/Teacher/AddExperiments"
-import UpdateExperiments from "../components/Teacher/UpdateExperiments"
-import ManageExperiments from "../components/Teacher/ManageExperiments"
 import Header from "../components/Common/Header"
 import SideBar from "../components/Common/SideBar"
-import DashBoard from "../components/SystemAdmin/DashBoard"
 import CAdvisor from "../components/SystemAdmin/CAdvisor"
 import EFK from "../components/SystemAdmin/EFK"
 import Settings from "../components/Common/Settings"
 import LoginPage from "../components/Common/LoginPage"
 import Ping from "../components/Common/ping"
-
-import ManageStudents from "../components/UserAdmin/ManageStudents"
-import UpdateStudents from "../components/UserAdmin/UpdateStudents"
-import AddStudents from "../components/UserAdmin/AddStudents"
-
-import ManageTeachers from "../components/UserAdmin/ManageTeachers"
-import UpdateTeachers from "../components/UserAdmin/UpdateTeachers"
-import AddTeachers from "../components/UserAdmin/AddTeachers"
-
-import ManageSysAdmins from "../components/UserAdmin/ManageSysAdmins"
-import UpdateSysAdmins from "../components/UserAdmin/UpdateSysAdmins"
-import AddSysAdmins from "../components/UserAdmin/AddSysAdmins"
-import AuthConfig from "../components/UserAdmin/AuthConfig"
-
 //student
-import studentcourse from "../components/Student/Studentcourselist"
-
-
-
-
 
 
 Vue.use(Router)
@@ -41,11 +17,6 @@ export default new Router({
     // 设置链接激活时使用的 CSS 类名
     linkActiveClass: "active",
     routes: [
-        {
-            path: '/',
-            name: 'ListExperiments',
-            component: ListExperiments
-        },
         {
             path: '/ping',
             name: 'Ping',
@@ -60,32 +31,6 @@ export default new Router({
             path: '/sideBar',
             name: 'SideBar',
             component: SideBar
-        },
-        {
-            path: '/experiment',
-            name: 'Experiment',
-            component: Experiment
-        },
-        {
-            path: '/teacher/add',
-            name: 'AddExperiments',
-            component: AddExperiments
-        },
-        {
-            path: '/teacher/manage',
-            name: 'ManageExperiments',
-            component: ManageExperiments
-        },
-        // UpdateExperiments这个组件暂时没有用到
-        {
-            path: '/teacher/update',
-            name: 'UpdateExperiments',
-            component: UpdateExperiments
-        },
-        {
-            path: '/dashboard',
-            name: 'DashBoard',
-            component: DashBoard
         },
         {
             path: '/cadvisor',
@@ -107,64 +52,5 @@ export default new Router({
             name: 'LoginPage',
             component: LoginPage
         },
-        {
-            path: '/useradmin/manage/student',
-            name: 'ManageStudents',
-            component: ManageStudents
-        },
-        {
-            path: '/useradmin/update/student',
-            name: 'UpdateStudents',
-            component: UpdateStudents
-        },
-        {
-            path: '/useradmin/add/student',
-            name: 'AddStudents',
-            component: AddStudents
-        },
-        {
-            path: '/useradmin/manage/teacher',
-            name: 'ManageTeachers',
-            component: ManageTeachers
-        },
-        {
-            path: '/useradmin/update/teacher',
-            name: 'UpdateTeachers',
-            component: UpdateTeachers
-        },
-        {
-            path: '/useradmin/add/teacher',
-            name: 'AddTeachers',
-            component: AddTeachers
-        },
-        {
-            path: '/useradmin/manage/sysAdmin',
-            name: 'ManageSysAdmins',
-            component: ManageSysAdmins
-        },
-        {
-            path: '/useradmin/update/sysAdmin',
-            name: 'UpdateSysAdmins',
-            component: UpdateSysAdmins
-        },
-        {
-            path: '/useradmin/add/sysAdmin',
-            name: 'AddSysAdmins',
-            component: AddSysAdmins
-        },
-        {
-            path: '/useradmin/config',
-            name: 'AuthConfig',
-            component: AuthConfig
-        },
-
-
-
-        //new student
-        {
-            path: '/student/course',
-            name: 'Studentcourselist',
-            component: studentcourse
-        }
     ]
 })
